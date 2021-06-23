07bc945446d63e91d0375fc5d95afe36f821f90a
CarlSchader <carlwschader@gmail.com>
2021-06-23 16:28:36 -0600

    changed all references to the feature part of versioning to minor

--- scripts/incrementMinor.js
+++ scripts/incrementMinor.js
@@ -2,8 +2,8 @@ const fs = require("fs");
 
 let packageJson = JSON.parse(fs.readFileSync("./package.json"));
 
-let [major, feature, patch] = packageJson.version.split(".");
+let [major, minor, patch] = packageJson.version.split(".");
 
-packageJson.version = `${major}.${parseInt(feature) + 1}.${0}`;
+packageJson.version = `${major}.${parseInt(minor) + 1}.${0}`;
 
 fs.writeFileSync("./package.json", JSON.stringify(packageJson, null, 2));
--- scripts/incrementPatch.js
+++ scripts/incrementPatch.js
@@ -2,8 +2,8 @@ const fs = require("fs");
 
 let packageJson = JSON.parse(fs.readFileSync("./package.json"));
 
-let [major, feature, patch] = packageJson.version.split(".");
+let [major, minor, patch] = packageJson.version.split(".");
 
-packageJson.version = `${major}.${feature}.${parseInt(patch) + 1}`;
+packageJson.version = `${major}.${minor}.${parseInt(patch) + 1}`;
 
 fs.writeFileSync("./package.json", JSON.stringify(packageJson, null, 2));
