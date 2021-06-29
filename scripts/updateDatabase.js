06e97a613b628831f37f12895ff1d5e7227638de
CarlSchader <carlwschader@gmail.com>
2021-06-29 13:34:54 -0600

    new tooling, first production build on cloud run

--- scripts/updateDatabase.js
+++ scripts/updateDatabase.js
@@ -5,7 +5,6 @@ require("dotenv").config();
 
 const schemas = JSON.parse(fs.readFileSync("./schemas.json"));
 const indexes = JSON.parse(fs.readFileSync("./indexes.json"));
-const examples = require("../exampleData.js");
 const { databaseName } = JSON.parse(fs.readFileSync("./config.json"));
 
 (async function() {
@@ -15,28 +14,22 @@ const { databaseName } = JSON.parse(fs.readFileSync("./config.json"));
     const db = client.db(databaseName);
 
     for (let schemaName in schemas) {
-        await db.createCollection(schemaName, {
+        await db.command({
+            collMod: schemaName,
             validator: {
                 $jsonSchema: schemas[schemaName]
             }
         });
-        console.log(`created collection: ${schemaName}`);
     }
 
     for (const collection in indexes) {
         for (const kvp of indexes[collection]) {
             await db.collection(collection).createIndex(kvp.index, kvp.options);
         }
-        console.log(`created indexes for collection: ${collection}`);
-    }
-
-    for (const collection in examples) {
-        await db.collection(collection).insertOne(examples[collection]);
-        console.log(`inserted example for collection: ${collection}`);
     }
 
     await client.close();
 
-    console.log(`database: ${databaseName} has been created`);
+    console.log(`database ${databaseName} has been updated`);
     process.exit(0);
 })();
